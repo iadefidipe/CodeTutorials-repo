3a8d0d3a86e7ce7810e8416f25ed67859a2e1ca2
Israel Adefidipe <israeladefidipe@gmail.com>
2021-05-09 23:54:11 +0100

    completed traversing the DOM, currently on DOM events

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 let i;
 
 // // this query elements using their  ID
-// const banner = document.getElementById ('page-banner');
+const banner = document.getElementById ('page-banner');
 
 // // this query elements using their tagname
-// const lis = document.getElementsByTagName('li');
+const lis = document.getElementsByTagName('li');
 
 
 // this query elements using their className
@@ -19,9 +19,9 @@ const titles = document.getElementsByClassName('title');
 
 // using the for loop
 
-// for( i=0; i<titles.length; i++){
-//     console.log(titles[i]);
-// }
+for( i=0; i<titles.length; i++){
+    console.log(titles[i]);
+}
 
 // using the forEach method
 // titles.forEach(function(item){
@@ -29,11 +29,11 @@ const titles = document.getElementsByClassName('title');
 // })
 // this wont work because tiles at this point is a HTML collection, so we have to convert it to an array first, to be able to use the forEach method 
 
-// console.log(Array.isArray(Array.from(titles)));
+console.log(Array.isArray(Array.from(titles)));
 
-// Array.from(titles).forEach(function(item){
-//     console.log(item)
-// })
+Array.from(titles).forEach(function(item){
+    console.log(item)
+})
 // now it works, because titles is now an array
 
 
@@ -41,24 +41,24 @@ const titles = document.getElementsByClassName('title');
 
 //  you can use the document.querySelector to selcect use CSS element selectors
 
-// const books = document.querySelectorAll('#book-list li .name');
+const books = document.querySelectorAll('#book-list li .name');
 
-// console.log(books)
+console.log(books);
 
-// querySelectorAll returns a nodelist and not a html collection, you can use the forEach method on the noidelist
+// querySelectorAll returns a nodelist and not a html collection, you can use the forEach method on the nodelist
 
-// Array.from(books).forEach( function(item){
-//     console.log(item)
-// })
+Array.from(books).forEach( function(item){
+    console.log(item)
+})
 
 
 // ********Text content
 // changing the text in HTML element using the .textContent property
 
 // const books = document.querySelectorAll('#book-list li .name');
-// books.forEach(function(items){
-//     items.textContent += " book title"
-// });
+books.forEach(function(items){
+    items.textContent += " book title"
+});
 
 // ***** innerHtML property
 //  this properety can be used to change the HTML inside any HTML elemnet it is used to target
@@ -69,7 +69,7 @@ const titles = document.getElementsByClassName('title');
 //  Every thing on the HTML page is a type of node 
 // Node types have number values
 
-const banner = document.querySelector('#page-banner');
+// const banner = document.querySelector('#page-banner');
 
 console.log(banner.nodeType); //this property returns the node value of the targeted node
 console.log(banner.nodeName); //returns the node name
@@ -77,6 +77,59 @@ console.log(banner.hasChildNodes()); //return true or false, if the node has chi
 
 // we can clone nodes, which is very simple by the way
 const cloneBanner = banner.cloneNode(true); //it is very important to pass the argument as true, if there is no true it wont clone the child nodes
-console.log(cloneBanner)
+console.log(cloneBanner);
+
+
+// ********Traversing the DOM
+// we can traverse the DOM with simple properties
+const bookList = document.querySelector('#book-list');
+
+// traversing upwards (child to parent)
+
+console.log('the parent node is:', bookList.parentNode);//this returns the direct parent of the selected node
+console.log('the parent element is:', bookList.parentElement); //does the same thing as parentNode
+console.log('the parent element is:', bookList.parentElement.parentElement); //this returns the parent of the parent node
+
+// traversing downwards (parent to child)
+
+console.log(bookList.childNodes); //The includes the line break that it returns as text nodes, which we dont necesarily need
+
+console.log(bookList.children); // we use this instead, it only returns the child nodes without the text nodes (so this will be used predominatly)
+
+// travesing from sibling to sibling (sibling elements are elemnts on the same level, like the different direct children elements of a parent)
+
+console.log('next sibling is:', bookList.nextSibling); //this returns the next sibling node
+console.log('next sibling is:', bookList.nextElementSibling); //this returns the next element node (so this will be used predominatly)
+
+console.log('previous sibling is:', bookList.previousSibling);
+console.log('previous sibling is:', bookList.previousElementSibling); //(so this will be used predominatly)
+
+
+const btns = document.querySelectorAll('#book-list .delete');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){ // e = event object
+
+        const li = e.target.parentElement; //the target property tagets the exact button that was clicked
+
+        li.parentNode.removeChild(li); //use to remove an element in JS 
+
+    })
+})
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', funtion(e){
+    e.preventDeault(); //this prevent normal default functions
+})
+
+
+
+
+
+
+
+
+
 
 
